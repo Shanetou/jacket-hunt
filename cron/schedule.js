17f51761310a297df9cf1ec5482563ca08e1cc10
Shane McQuerter <shanemcquerter@gmail.com>
2021-01-12 17:42:02 -0800

    run scraper every minute to test heroku deploy

--- cron/schedule.js
+++ cron/schedule.js
@@ -1,7 +1,8 @@
 const spawn = require("child_process").spawn;
 const schedule = require("node-schedule");
 
-schedule.scheduleJob("0 0 6 * * ?", () => {
+// schedule.scheduleJob("0 0 6 * * ?", () => {
+schedule.scheduleJob("* * * * *", () => {
   console.log("Launching patagooch scraper.\n");
 
   spawn("node", ["index.js"]);
