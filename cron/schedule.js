d77f0f89317c69d618b433c1ca62de2a24dc6ad7
Shane McQuerter <shanemcquerter@gmail.com>
2021-01-12 21:13:06 -0800

    remove worker and use heroku scheduler

--- cron/schedule.js
+++ cron/schedule.js
@@ -1,7 +1,8 @@
 const spawn = require("child_process").spawn;
 const schedule = require("node-schedule");
 
-schedule.scheduleJob("* * * * *", () => {
+// run at 6am daily
+schedule.scheduleJob("0 0 6 * * ?", () => {
   console.log("Launching patagooch scraper.\n");
 
   spawn("node", ["index.js"]);
