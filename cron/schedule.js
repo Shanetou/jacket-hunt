c36588b0c8456a3424d154b7faa90705da0894cf
Shane McQuerter <shanemcquerter@gmail.com>
2021-01-12 20:02:16 -0800

    add Procfile for Heroku

--- cron/schedule.js
+++ cron/schedule.js
@@ -1,7 +1,6 @@
 const spawn = require("child_process").spawn;
 const schedule = require("node-schedule");
 
-// schedule.scheduleJob("0 0 6 * * ?", () => {
 schedule.scheduleJob("* * * * *", () => {
   console.log("Launching patagooch scraper.\n");
 
